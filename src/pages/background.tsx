0bc2f7c2b52929af80dd86254adc32442ffaef8f
Patryk Szydłowski <patrykszydlowski99@gmail.com>
2022-07-17 19:12:52 +0200

    feat: add a mix of SSG & SSR

--- src/pages/background.tsx
+++ src/pages/background.tsx
@@ -1,3 +1,7 @@
+import { type GetStaticProps } from "next";
+
 import { Background } from "@/design-system/components/background";
 
 export default () => <Background color="secondary">I only use the background component</Background>;
+
+export const getStaticProps: GetStaticProps = async () => ({ props: {} });
--- src/pages/text.tsx
+++ src/pages/text.tsx
@@ -1,3 +1,6 @@
+import { type GetServerSideProps } from "next";
+
 import { Text } from "@/design-system/components/text";
 
 export default () => <Text color="secondary">I only use the text component</Text>;
+export const getServerSideProps: GetServerSideProps = async () => ({ props: {} });
