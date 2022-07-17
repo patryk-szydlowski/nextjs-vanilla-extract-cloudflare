c59fdef3675152ea6ff6b7e1238a667b25e6ec01
Patryk Szydłowski <patrykszydlowski99@gmail.com>
2022-07-17 18:25:37 +0200

    chore: initialize NextJS repo

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+module.exports = {
+  experimental: {
+    reactRoot: true,
+    runtime: "edge",
+    serverComponents: true,
+  },
+  reactStrictMode: true,
+};
